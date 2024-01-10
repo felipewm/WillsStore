0e41b014e6a76503d3302a03e5e5cc24a029e0e1
felipewm <fwmartirani@gmail.com>
2024-01-10 17:20:59 -0300

    meu primeiro commit

---
+++ src/WillsStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WillsStore.Core.DomainObjects;
+
+namespace WillsStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAgregateRoot
+    {
+
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+
+        public string Descricao { get; private set; }
+
+        public bool Ativo { get; private set; }
+
+        public decimal Valor { get; private set; }
+
+        public DateTime DataCadastro { get; private set; }
+
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public Dimensoes Dimensoes { get; private set; }
+
+        public Categoria Categoria { get; private set; }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, Dimensoes dimensoes)
+        {
+            CategoriaId = categoriaId;
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+            Dimensoes = dimensoes;
+        }
+
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+        public void DebitarEstoque(int quantidade)
+        {
+            if (quantidade < 0) quantidade *= -1;
+            if (!PossuiEstoque(quantidade)) throw new DomainException("Estoque insuficiente");
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo nome do produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Descricao, "O campo descrição não pode estar vazio");
+            Validacoes.ValidarSeDiferente(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
+            Validacoes.ValidarSeMenorIgual(Valor, 0, "O valor do produto não pode ser menor igual a 0");
+            Validacoes.ValidarSeVazio(Imagem, "O campo imagem não pode estar vazio");
+        }
+    }
+
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+
+        public string Codigo { get; private set;}
+
+        public Categoria(string nome, string codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}"; 
+        }
+    }
+
+}
